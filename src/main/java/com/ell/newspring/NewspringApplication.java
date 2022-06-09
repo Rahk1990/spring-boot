6cae83ea266571324738a1df24aca9e34221a4a4
rahk1990 <rahkem.ellis@gmail.com>
2022-06-08 20:01:04 -0400

    initial files added for spring frame work

---
+++ src/main/java/com/ell/newspring/NewspringApplication.java
@@ -0,0 +1,13 @@
+package com.ell.newspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ell/newspring/NewspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ell.newspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
